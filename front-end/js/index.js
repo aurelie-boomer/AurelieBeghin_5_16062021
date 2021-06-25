5ac8dfc7d2a9e97be70df08f98c3110bd0c6d173
aurelie-boomer <aurelie.thks@gmail.com>
2021-06-25 16:18:41 +0200

    mise en oplace du mobile first

---
+++ front-end/js/index.js
@@ -0,0 +1,22 @@
+fetch("http://localhost:3000/api/cameras")
+    .then((resultat) => {
+        return resultat.json();
+    })
+    .then((cameras) => {
+        for (const camera of cameras) {
+            console.log(camera);
+            const template = document.getElementById("cameraTemplate");
+            const clonedTemplate = document.importNode(template.content, true);
+
+            clonedTemplate.getElementById("cameraImg").src = camera.imageUrl;
+            clonedTemplate.getElementById("cameraName").textContent =
+                camera.name;
+            clonedTemplate.getElementById("cameraDescription").textContent =
+                camera.description;
+
+            clonedTemplate.getElementById("cameraPrice").textContent =
+                camera.price / 100 + "€";
+
+            document.getElementById("camerasList").appendChild(clonedTemplate);
+        }
+    }); 
