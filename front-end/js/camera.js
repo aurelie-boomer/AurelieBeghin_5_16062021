4f4c3dc0a75de0ea83cc88d1df9eaff8018eba87
aurelie-boomer <aurelie.thks@gmail.com>
2021-07-06 09:55:53 +0200

    parcourir les lentilles

--- front-end/js/camera.js
+++ front-end/js/camera.js
@@ -18,11 +18,12 @@ fetch("http://localhost:3000/api/cameras/" + cameraId)
         camera.price / 100 + "€";
         document.getElementById("cameraDescription").innerHTML =
             "<ul><li>" + camera.description + "</li></ul>";
+        //document.getElementById("cameraLenses").textContent = camera.lenses;
 
 
 /**********************************************************************************************/
-// parcourir les lentilles (camera.lenses)
-            for (const lenses of camera.keys(camera)) {
+// parcourir les lentilles (camera.lenses)   TABLEAU??
+           /* for (const lenses of camera.keys(camera)) {
                 console.log(lenses);
                 var selectElmt = document.getElementById("cameraLenses");
                 var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
@@ -30,6 +31,7 @@ fetch("http://localhost:3000/api/cameras/" + cameraId)
             }
 
             addLenses
+            //template??
 
  // pour chaque lentille : ajouter un <option value="laLentille">laLentille</option> qui contient la lentille au select
 // grâce au .innerHTML qui permet d'injecter du code HTML depuis le javascript
@@ -44,9 +46,22 @@ fetch("http://localhost:3000/api/cameras/" + cameraId)
 
         
         // document.getElementById("cameraLenses");
+        //Fonction pour le tableau lenses
+           const lenseList = () => {
+        for (let i = 0; i < camera.lenses.length; i++) {
+        const option = document.createElement("option") //Créé notre liste option
+        option.setAttribute("value", camera.lenses[i]) //Incrémente nos lenses à notre liste option               
+        option.innerHTML = camera.lenses[i]
+        lenses.appendChild(option)
+    }
+}
 
         // puis dans le javascript, écouter son évènement "click"
+       /*const lense = document.getElementById("laLentille");
+       addlense.addEventListener("click",function(){
+           console log("addlense", i);
 
+       });*/
 
         // et au clic, ajouter au localStorage
     });
