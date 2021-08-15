1da433f41d29c4b00c6d4ff90e5825a848574da8
aurelie-boomer <aurelie.thks@gmail.com>
2021-08-15 21:11:26 +0200

    finalisation du css

---
+++ front-end/js/confirmation.js
@@ -0,0 +1,9 @@
+const url = new URL(window.location.href);
+const searchParams = url.searchParams;
+const firstName = searchParams.get("name");
+const orderId = searchParams.get("orderId");
+const totalPrice = searchParams.get("totalPrice");
+
+document.getElementById("name").textContent = firstName;
+document.getElementById("orderId").textContent = orderId;
+document.getElementById("totalPrice").textContent = totalPrice;
--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -1,5 +1,5 @@
 import { getCart, saveCart } from "./utils/cart.js";
-main(); //récupérer le panier
+main(); //Récupérer le panier
 function main() {
   //Convertir les données (format JSON) qui sont dans le local storage en objet javascript
   const cart = getCart();
@@ -19,7 +19,7 @@ function fillCartInformation(cart) {
       });
   }
 }
-// additionner les prix des caméras ensemble
+// Additionner les prix des caméras ensemble
 async function fillTotalPrice() {
   let totalPrice = 0;
   const cart = getCart();
@@ -56,91 +56,144 @@ function addCameraToCamerasList(selectedCamera, camera) {
 
 //************** Validation formulaire ********************
 
-//récupération du formulaire
+//Récupération du formulaire
 const formContact = document.getElementById("formContact");
+//Les différents champs
+setupInputValidation(
+  "lastName",
+  "errorLastName",
+  /^[a-zA-Z-\séèäêîï]{3,20}$/,
+  "Le champ est invalide"
+);
+setupInputValidation(
+  "firstName",
+  "errorFirstName",
+  /^[a-zA-Z-\séèäêîï]{3,20}$/,
+  "Le champ est invalide"
+);
+setupInputValidation(
+  "city",
+  "errorCity",
+  /^[a-zA-Z-\séèäêîï]{3,20}$/,
+  "Le champ est invalide"
+);
+setupInputValidation(
+  "address",
+  "errorAddress",
+  /^[0-9a-zA-Z-\séèäêîï]{3,20}$/,
+  "Le champ est invalide"
+);
+setupInputValidation(
+  "email",
+  "errorEmail",
+  /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/,
+  "Le champ est invalide"
+);
 
-// vérification des champs avant envoi
-//************** Nom ********************
-formContact.addEventListener("submit", function (e) {
-  const champLastName = document.getElementById("lastName");
-  const myRegex = /^[a-zA-Z-\séèäêîï]{3,20}+$/;
-  if (champLastName.value == "") {
-    const error = document.getElementById("error");
+// validation des champs avant envoi
+function setupInputValidation(id, errorId, regex, errorMessage) {
+  const input = document.getElementById(id);
+  input.addEventListener("change", function (e) {
+    validateInput(id, errorId, regex, errorMessage);
+  });
+}
+
+// Validation du formulaire avant envoi
+
+document.getElementById("submitButton").addEventListener("click", function (e) {
+  if (
+    !validateInput(
+      "lastName",
+      "errorLastName",
+      /^[a-zA-Z-\séèäêîï]{3,20}$/,
+      "Le champ est invalide"
+    ) ||
+    !validateInput(
+      "firstName",
+      "errorFirstName",
+      /^[a-zA-Z-\séèäêîï]{3,20}$/,
+      "Le champ est invalide"
+    ) ||
+    !validateInput(
+      "city",
+      "errorCity",
+      /^[a-zA-Z-\séèäêîï]{3,20}$/,
+      "Le champ est invalide"
+    ) ||
+    !validateInput(
+      "address",
+      "errorAddress",
+      /^[0-9a-zA-Z-\séèäêîï]{3,20}$/,
+      "Le champ est invalide"
+    ) ||
+    !validateInput(
+      "email",
+      "errorEmail",
+      /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/,
+      "Le champ est invalide"
+    )
+  ) {
+    return;
+  }
+  // envoyer au back-end pour recevoir un numéro de commande --> post
+  // Récupération des informations de livraison
+  const contact = new Object();
+  contact.lastName = document.getElementById("lastName").value;
+  contact.firstName = document.getElementById("firstName").value;
+  contact.address = document.getElementById("address").value;
+  contact.city = document.getElementById("city").value;
+  contact.email = document.getElementById("email").value;
+
+  const cart = getCart();
+  const products = [];
+  // on sélectionne les id
+  for (const selectedCamera of cart) {
+    products.push(selectedCamera.id);
+  }
+
+  const httpResponse = fetch("http://localhost:3000/api/cameras/order", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    // Transformer l'objet js en JSON (chaine de caractères)
+    body: JSON.stringify({
+      contact: contact, // bien passer ton objet contact
+      products: products, // bien passer ton tableau products
+    }),
+  })
+    .then((httpResponse) => httpResponse.json())
+    .then((response) => {
+      console.log(response);
+      const orderId = response.orderId;
+      localStorage.removeItem("cart");
+      window.location.href =
+        "confirmation.html?name=" +
+        contact.firstName +
+        "&totalPrice=" +
+        document.getElementById("totalPrice").textContent +
+        "&orderId=" +
+        orderId;
+    });
+});
+
+function validateInput(id, errorId, regex, errorMessage) {
+  const input = document.getElementById(id);
+  const error = document.getElementById(errorId);
+  if (input.value == "") {
     error.innerHTML = "Le champ est vide";
     error.style.color = "red";
-    e.preventDefault();
-  } else if (myRegex.test(champLastName.value) == false) {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ n'est pas valide";
+    return false;
+  } else if (regex.test(input.value) == false) {
+    error.innerHTML = errorMessage;
     error.style.color = "red";
-    e.preventDefault();
+    return false;
   }
-});
-//************** Prénom ********************
-formContact.addEventListener("submit", function (e) {
-  const champFirstName = document.getElementById("FirstName");
-  const myRegex = /^[a-zA-Z-\séèäêîï]{3,20}+$/;
-  if (champFirstName.value == "") {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ est vide";
-    error.style.color = "red";
-    e.preventDefault();
-  } else if (myRegex.test(champFirstName.value) == false) {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ n'est pas valide";
-    error.style.color = "red";
-    e.preventDefault();
-  }
-});
-//************** Ville ********************
-formContact.addEventListener("submit", function (e) {
-  const champCity = document.getElementById("city");
-  const myRegex = /^[a-zA-Z-\séèäêîï]{3,20}+$/;
-  if (champCity.value == "") {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ est vide";
-    error.style.color = "red";
-    e.preventDefault();
-  } else if (myRegex.test(champCity.value) == false) {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ n'est pas valide";
-    error.style.color = "red";
-    e.preventDefault();
-  }
-});
-//************** Adresse ********************
-formContact.addEventListener("submit", function (e) {
-  const champAddress = document.getElementById("address");
-  const myRegex = /^[0-9a-zA-Z-\séèäêîï]{3,20}+$/;
-  if (champAdress.value == "") {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ est vide";
-    error.style.color = "red";
-    e.preventDefault();
-  } else if (myRegex.test(champAddress.value) == false) {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ n'est pas valide";
-    error.style.color = "red";
-    e.preventDefault();
-  }
-});
-//************** Email ********************
-formContact.addEventListener("submit", function (e) {
-  const champEmail = document.getElementById("email");
-  const myRegex =
-    /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$/;
-  if (champEmail.value == "") {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ est vide";
-    error.style.color = "red";
-    e.preventDefault();
-  } else if (myRegex.test(champEmail.value) == false) {
-    const error = document.getElementById("error");
-    error.innerHTML = "Le champ n'est pas valide";
-    error.style.color = "red";
-    e.preventDefault();
-  }
-});
+  //Pour effacer l'erreur si correction
+  error.innerHTML = "";
+  return true;
+}
 
 //************** Bouton pour supprimer un produit du panier et l enlever du localstorage ********************
 
@@ -157,9 +210,9 @@ function removeProductFromCart(product) {
   if (productIndex === -1) {
     return false;
   }
-  //supprimer l'élément du tableau
+  //Supprimer l'élément du tableau
   cart.splice(productIndex, 1);
-  //enregistrer le nouveau panier
+  //Enregistrer le nouveau panier
   saveCart(cart);
   return true;
 }
@@ -169,13 +222,11 @@ btnDeleteBasket.addEventListener("click", function (e) {
   e.preventDefault;
   localStorage.removeItem("cart");
   alert("Le panier a été vidé ");
-  //faire la suppression visuelle
+  //Faire la suppression visuelle
   const tableProducts = document.getElementById("camerasList");
   tableProducts.innerHTML = "";
-  //enregistrer le nouveau panier
-  saveCart(cart);
-  return true;
+  // Mettre le panier à Zéro après l'avoir vidé
+  const DeleteTotalPrice = document.getElementById("totalPrice");
+  DeleteTotalPrice.textContent = "0€";
+  // Supprimer le panier sans rafraîchir
 });
-
-// Refaire un clone en haut comme pour la suppression d un article???
-// Total a Zéro??
